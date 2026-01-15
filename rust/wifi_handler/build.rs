c9fd6fd79e2e5f31e04604db761c2dc1e46ecca8
Simon Kagstrom <simon.kagstrom@gmail.com>
2026-01-15 07:10:22 +0100

    bindgen-wip

--- rust/wifi_handler/build.rs
+++ rust/wifi_handler/build.rs
@@ -1,15 +1,19 @@
 fn main() -> miette::Result<()> {
-    let include_path = std::path::PathBuf::from("../../esp32/esp32p4/main/");
+    let include_path = "../../src/include"; // path to your C++ headers
 
-    // This assumes all your C++ bindings are in main.rs
     let mut b = autocxx_build::Builder::new("src/lib.rs", &[&include_path])
-//        .extra_clang_args(&["-mabi=ilp32f"])
-        .build().unwrap();
+    .extra_clang_args(&["-x", "c++", "-std=c++23"])
+        .extra_clang_args(&["--target", "riscv32imafc-unknown-none-elf"])
+        .extra_clang_args(&["-isystem", "/Users/ska/.espressif/tools/riscv32-esp-elf/esp-14.2.0_20251107/riscv32-esp-elf/bin/../riscv32-esp-elf/include",
+        "-isystem", "/Users/ska/.espressif/tools/riscv32-esp-elf/esp-14.2.0_20251107/riscv32-esp-elf/bin/../riscv32-esp-elf/include/c++/14.2.0",
+        "-isystem", "/Users/ska/.espressif/tools/riscv32-esp-elf/esp-14.2.0_20251107/riscv32-esp-elf/bin/../riscv32-esp-elf/include/c++/14.2.0/riscv32-esp-elf/"])
+        .build()?;
 
     b.flag("-march=rv32imafc")
-//        .flag("-mabi=ilp32f")
+        //        .flag("-mabi=ilp32f")
         .flag("-std=c++23")
         .compile("autocxx-demo"); // arbitrary library name, pick anything
+    
     println!("cargo:rerun-if-changed=src/lib.rs");
 
     // Add instructions to link to any C++ libraries you need.
--- rust/wifi_handler/src/lib.rs
+++ rust/wifi_handler/src/lib.rs
@@ -1,16 +1,23 @@
 use autocxx::prelude::*; // use all the main autocxx functions
 
 include_cpp! {
-    #include "kalkon.hh" // your header file name
-    safety!(unsafe) // see details of unsafety policies described in the 'safety' section of the book
-    generate!("kalkon") // add this line for each function or type you wish to generate
+    #include "hal/i_wifi.hh" // your header file name
+    safety!(unsafe_ffi) // see details of unsafety policies described in the 'safety' section of the book
+    generate!("IWifi") // add this line for each function or type you wish to generate
+}
+
+#[cxx::bridge]
+mod ffi_bridge {
+    extern "Rust" {
+        fn rust_main(wifi: &mut IWifi) -> i32;
+    }
 }
 
 #[unsafe(no_mangle)]
-extern "C" fn rust_main() -> i32 {
+extern "C" fn rust_main(wifi: &mut ffi::IWifi) -> i32 {
     log::info!("Hello, world!");
     println!("Hello from Rust! SIMON");
 
-    ffi::kalkon().into()
-//    42
+    //ffi::kalkon().into()
+    42
 }
