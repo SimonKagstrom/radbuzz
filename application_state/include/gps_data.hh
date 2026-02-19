53ba92c479dbadfcb6583dc9126d90a3a802f836
Simon Kagstrom <simon.kagstrom@gmail.com>
2026-02-19 21:14:30 +0100

    application_state: Add header dependencies

---
+++ application_state/include/gps_data.hh
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "hal/i_gps.hh"
+#include "wgs84_to_osm_point.hh"
+
+struct GpsData
+{
+    GpsPosition position;
+    Point pixel_position;
+
+    float speed;
+    float heading;
+
+    // Add time, height, etc.
+};
