e93f27dd37fe358abaf5028f1d446fa910dcecaa
Simon Kagstrom <simon.kagstrom@gmail.com>
2026-04-07 14:56:26 +0200

    esp32: Adaptations for esp-idf 6.0

--- esp32/ble_server_esp32/include/ble_server_esp32.hh
+++ esp32/ble_server_esp32/include/ble_server_esp32.hh
@@ -10,6 +10,7 @@
 #include <nimble/nimble_port.h>
 #include <nimble/nimble_port_freertos.h>
 #include <nvs_flash.h>
+#include <optional>
 #include <services/gap/ble_svc_gap.h>
 #include <services/gatt/ble_svc_gatt.h>
 #include <vector>
