c51b39e28d67b60c6f4e64d41578591bc59b98e0
Simon Kagstrom <simon.kagstrom@gmail.com>
2026-04-18 10:01:09 +0200

    user_interface: Awake on updated speed

--- src/user_interface/user_interface.cc
+++ src/user_interface/user_interface.cc
@@ -26,6 +26,7 @@ UserInterface::UserInterface(hal::IDisplay& display,
                                               AS::distance_traveled,
                                               AS::bluetooth_connected,
                                               AS::wifi_connected,
+                                              AS::speed,
                                               AS::wh_consumed,
                                               AS::wh_regenerated>(GetSemaphore());
     m_cache_listener = m_image_cache.ListenToChanges(GetSemaphore());
