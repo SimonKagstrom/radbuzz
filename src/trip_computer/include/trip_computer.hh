cf552cb4d565121c8161038e17f5ab2c5737fb3a
Simon Kagstrom <simon.kagstrom@gmail.com>
2026-06-08 19:06:03 +0200

    trip_computer: Plan for trip log

--- src/trip_computer/include/trip_computer.hh
+++ src/trip_computer/include/trip_computer.hh
@@ -11,6 +11,16 @@ public:
     explicit TripComputer(ApplicationState& app_state);
 
 private:
+    struct TripLogEntry
+    {
+        Point position;
+        milliseconds timestamp;
+        int16_t power;
+    };
+
+    // 2MB of trip log entries
+    static constexpr auto kNumberOfTripLogEntries = (2 * 1024 * 1024) /  sizeof(TripLogEntry);
+
     std::optional<milliseconds> OnActivation() final;
 
     void UpdateSoc(uint16_t millivolts);
@@ -20,7 +30,8 @@ private:
     std::unique_ptr<ListenerCookie> m_state_listener;
     os::TimerHandle m_soc_timer;
 
-    etl::circular_buffer<uint16_t, 10> m_millivolt_history;
-
     uint32_t m_current_distance {0};
+
+    etl::circular_buffer<uint16_t, 10> m_millivolt_history;
+    std::unique_ptr<etl::circular_buffer<TripLogEntry, kNumberOfTripLogEntries>> m_trip_log;
 };
