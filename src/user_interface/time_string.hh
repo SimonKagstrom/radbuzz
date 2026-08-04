2d0ee86f9f2f9a73848e8c3d117d990e5bf83a82
Simon Kagstrom <simon.kagstrom@gmail.com>
2026-08-04 08:56:19 +0200

    user_interface: Add a seconds-to-string helper

---
+++ src/user_interface/time_string.hh
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <format>
+#include <string>
+
+static inline std::string
+SecondsToString(auto seconds)
+{
+    if (seconds >= 60)
+    {
+        if (seconds >= 3600)
+        {
+            // Hours
+            return std::format(
+                "{:02}:{:02}:{:02}", seconds / 3600, (seconds % 3600) / 60, seconds % 60);
+        }
+        else
+        {
+            // Minutes
+            return std::format("{:02}:{:02}", seconds / 60, seconds % 60);
+        }
+    }
+
+    return std::format("{}", seconds);
+}
