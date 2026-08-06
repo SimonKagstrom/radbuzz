d45026c0be8c4f7429ad7d08d838bfbca28a13d0
Simon Kagstrom <simon.kagstrom@gmail.com>
2026-08-06 15:48:43 +0200

    indicators: Add a small dot for the battery icon to indicate BMS activeness

--- src/user_interface/indicators.hh
+++ src/user_interface/indicators.hh
@@ -1,15 +1,27 @@
 #pragma once
 
+#include "radbuzz_font_22.h"
 #include "user_interface.hh"
 
 class BatteryIndicator final : public UserInterface::IndicatorBase
 {
 public:
-    using UserInterface::IndicatorBase::IndicatorBase;
+    BatteryIndicator(UserInterface& parent, const Point& position)
+        : UserInterface::IndicatorBase(parent, position)
+        , m_bms_label(lv_label_create(lv_layer_top()))
+    {
+        lv_obj_set_style_text_font(m_bms_label, &radbuzz_font_22, LV_PART_MAIN);
+        lv_obj_set_style_bg_opa(m_bms_label, LV_OPA_TRANSP, LV_PART_MAIN);
+        lv_label_set_recolor(m_bms_label, true);
+        lv_label_set_text(m_bms_label, "•");
+
+        lv_obj_align_to(m_bms_label, m_indicator_label, LV_ALIGN_OUT_LEFT_MID, -4, -4);
+    }
 
     void Update(ApplicationState& state) final
     {
         const uint8_t battery_soc = state.Get<AS::battery_soc>();
+        const char* bms_text = "#4CAF50 •#";
 
         if (battery_soc > 90)
         {
@@ -26,18 +38,25 @@ public:
         else if (battery_soc > 20)
         {
             m_text = std::format("#ffa500 {}#", LV_SYMBOL_BATTERY_1);
+            bms_text = "#ffa500 •#";
         }
         else
         {
             m_text = std::format("#F44336 {}#", LV_SYMBOL_BATTERY_EMPTY);
+            bms_text = "#F44336 •#";
         }
         lv_label_set_text(m_indicator_label, m_text.c_str());
+        lv_label_set_text(m_bms_label, bms_text);
 
         lv_obj_set_flag(m_indicator_label, LV_OBJ_FLAG_HIDDEN, m_parent.OnTripMeterScreen());
+        lv_obj_set_flag(m_bms_label,
+                        LV_OBJ_FLAG_HIDDEN,
+                        m_parent.OnTripMeterScreen() || !state.Get<AS::bms_data>()->valid);
     }
 
 private:
     std::string m_text;
+    lv_obj_t* m_bms_label {nullptr};
 };
 
 class WifiIndicator final : public UserInterface::IndicatorBase
