b5e934d729923099f6d2ad1f3b159254b18f3bbb
Simon Kagstrom <simon.kagstrom@gmail.com>
2026-08-15 08:47:47 +0200

    speedometer_only_screen: Partial refactor to make units smaller

--- src/user_interface/speedometer_only_screen.cc
+++ src/user_interface/speedometer_only_screen.cc
@@ -6,6 +6,119 @@
 #include "radbuzz_font_40.h"
 #include "time_string.hh"
 
+namespace
+{
+
+auto
+CreateDatum(lv_obj_t* screen)
+{
+    SpeedometerOnlyScreen::Datum datum;
+
+    datum.description_label = lv_label_create(screen);
+    lv_obj_set_style_text_font(datum.description_label, &radbuzz_font_22, LV_PART_MAIN);
+    lv_obj_set_style_text_color(datum.description_label, lv_color_white(), LV_PART_MAIN);
+
+    datum.value_unit_label = lv_label_create(screen);
+    lv_obj_set_style_text_font(datum.value_unit_label, &radbuzz_font_22, LV_PART_MAIN);
+    lv_obj_set_style_text_color(datum.value_unit_label, lv_color_white(), LV_PART_MAIN);
+
+    datum.value_label = lv_label_create(screen);
+    lv_obj_set_style_text_font(datum.value_label, &radbuzz_font_40, LV_PART_MAIN);
+    lv_obj_set_style_text_color(datum.value_label, lv_color_white(), LV_PART_MAIN);
+    lv_obj_set_style_text_align(datum.value_label, LV_TEXT_ALIGN_RIGHT, LV_PART_MAIN);
+
+    return datum;
+}
+
+} // namespace
+
+SpeedometerOnlyScreen::Datum
+SpeedometerOnlyScreen::TopRight(const char* label_text, const char* unit_text)
+{
+    auto datum = CreateDatum(m_screen);
+
+    lv_obj_align(datum.description_label, LV_ALIGN_TOP_RIGHT, -20, 4);
+    lv_label_set_text(datum.description_label, label_text);
+
+    lv_obj_align_to(datum.value_unit_label,
+                    datum.description_label,
+                    LV_ALIGN_OUT_RIGHT_BOTTOM,
+                    -20,
+                    kPixelSize_radbuzz_font_40 + 4);
+    lv_label_set_text(datum.value_unit_label, unit_text);
+
+    lv_obj_set_style_text_align(datum.value_label, LV_TEXT_ALIGN_RIGHT, LV_PART_MAIN);
+
+    return datum;
+}
+
+SpeedometerOnlyScreen::Datum
+SpeedometerOnlyScreen::TopLeft(const char* label_text, const char* unit_text)
+{
+    auto datum = CreateDatum(m_screen);
+
+    lv_obj_align(datum.description_label, LV_ALIGN_TOP_LEFT, 0, 4);
+    lv_label_set_text(datum.description_label, label_text);
+
+    lv_label_set_text(datum.value_unit_label, unit_text);
+    lv_obj_align_to(datum.value_label, datum.description_label, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 4);
+    lv_obj_align_to(datum.value_unit_label, datum.value_label, LV_ALIGN_OUT_RIGHT_BOTTOM, 0, -4);
+
+    lv_obj_set_style_text_align(datum.value_label, LV_TEXT_ALIGN_RIGHT, LV_PART_MAIN);
+
+    return datum;
+}
+
+SpeedometerOnlyScreen::Datum
+SpeedometerOnlyScreen::BottomLeft(const char* label_text,
+                                  const char* value_text,
+                                  const char* unit_text)
+{
+    auto datum = CreateDatum(m_screen);
+
+    lv_obj_align(datum.value_label, LV_ALIGN_BOTTOM_LEFT, 0, -10);
+    lv_label_set_text(datum.description_label, label_text);
+
+    lv_label_set_text(datum.value_label, value_text);
+    lv_label_set_text(datum.value_unit_label, unit_text);
+
+    lv_obj_align_to(datum.description_label, datum.value_label, LV_ALIGN_OUT_TOP_LEFT, 0, -4);
+    lv_obj_align_to(datum.value_unit_label, datum.value_label, LV_ALIGN_OUT_RIGHT_BOTTOM, 4, -4);
+
+    lv_obj_set_style_text_align(datum.value_label, LV_TEXT_ALIGN_RIGHT, LV_PART_MAIN);
+
+    return datum;
+}
+
+SpeedometerOnlyScreen::Datum
+SpeedometerOnlyScreen::BottomRight(const char* label_text,
+                                   const char* value_text,
+                                   const char* unit_text)
+{
+    auto datum = CreateDatum(m_screen);
+
+    lv_label_set_text(datum.description_label, label_text);
+    lv_label_set_text(datum.value_label, value_text);
+    lv_label_set_text(datum.value_unit_label, unit_text);
+
+    lv_obj_set_style_text_align(datum.value_label, LV_TEXT_ALIGN_RIGHT, LV_PART_MAIN);
+    lv_obj_set_style_text_align(datum.description_label, LV_TEXT_ALIGN_RIGHT, LV_PART_MAIN);
+    lv_obj_set_style_text_align(datum.value_unit_label, LV_TEXT_ALIGN_RIGHT, LV_PART_MAIN);
+
+    lv_obj_align(datum.description_label,
+                 LV_ALIGN_BOTTOM_RIGHT,
+                 -20,
+                 -kPixelSize_radbuzz_font_40 - kPixelSize_radbuzz_font_22);
+    lv_obj_align_to(datum.value_unit_label,
+                    datum.description_label,
+                    LV_ALIGN_OUT_RIGHT_BOTTOM,
+                    -20,
+                    kPixelSize_radbuzz_font_40 + 4);
+    lv_obj_align_to(datum.value_label, datum.value_unit_label, LV_ALIGN_OUT_LEFT_BOTTOM, 0, 4);
+
+    return datum;
+}
+
 SpeedometerOnlyScreen::SpeedometerOnlyScreen(UserInterface& parent)
     : UserInterface::ScreenBase(parent, lv_obj_create(nullptr))
 {
@@ -26,80 +139,17 @@ SpeedometerOnlyScreen::SpeedometerOnlyScreen(UserInterface& parent)
     lv_obj_set_style_text_color(m_speedometer_unit_label, lv_color_white(), LV_PART_MAIN);
     lv_label_set_text(m_speedometer_unit_label, "km/h");
 
+    m_battery = TopLeft("Battery", "%");
+    m_temperature = TopRight("Controller/Motor/BMS/Cell", "°C");
+    m_trip_distance = BottomLeft("Trip", "999", "km");
 
-    m_battery_label = lv_label_create(m_screen);
-    lv_obj_align(m_battery_label, LV_ALIGN_TOP_LEFT, 0, 4);
-    lv_obj_set_style_text_font(m_battery_label, &radbuzz_font_22, LV_PART_MAIN);
-    lv_obj_set_style_text_color(m_battery_label, lv_color_white(), LV_PART_MAIN);
-    lv_label_set_text(m_battery_label, "Battery");
-
-    m_battery_value_label = lv_label_create(m_screen);
-    lv_obj_align_to(m_battery_value_label, m_battery_label, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 4);
-    lv_obj_set_style_text_font(m_battery_value_label, &radbuzz_font_40, LV_PART_MAIN);
-    lv_obj_set_style_text_color(m_battery_value_label, lv_color_white(), LV_PART_MAIN);
-    lv_label_set_text(m_battery_value_label, "100");
-
-    m_battery_value_unit_label = lv_label_create(m_screen);
-    lv_obj_align_to(
-        m_battery_value_unit_label, m_battery_value_label, LV_ALIGN_OUT_RIGHT_BOTTOM, 0, -4);
-    lv_obj_set_style_text_font(m_battery_value_unit_label, &radbuzz_font_22, LV_PART_MAIN);
-    lv_obj_set_style_text_color(m_battery_value_unit_label, lv_color_white(), LV_PART_MAIN);
-    lv_label_set_text(m_battery_value_unit_label, "%");
-
-    m_temperature_label = lv_label_create(m_screen);
-    lv_obj_align(m_temperature_label, LV_ALIGN_TOP_RIGHT, -20, 4);
-    lv_obj_set_style_text_font(m_temperature_label, &radbuzz_font_22, LV_PART_MAIN);
-    lv_obj_set_style_text_color(m_temperature_label, lv_color_white(), LV_PART_MAIN);
-    lv_label_set_text(m_temperature_label, "Controller/Motor/BMS/Cell");
-
-    m_temperature_value_unit_label = lv_label_create(m_screen);
-    lv_obj_align_to(m_temperature_value_unit_label,
-                    m_temperature_label,
-                    LV_ALIGN_OUT_RIGHT_BOTTOM,
-                    -20,
-                    kPixelSize_radbuzz_font_40 + 4);
-    lv_obj_set_style_text_font(m_temperature_value_unit_label, &radbuzz_font_22, LV_PART_MAIN);
-    lv_obj_set_style_text_color(m_temperature_value_unit_label, lv_color_white(), LV_PART_MAIN);
-    lv_label_set_text(m_temperature_value_unit_label, "°C");
-
-    m_temperature_value_label = lv_label_create(m_screen);
-    lv_obj_set_style_text_font(m_temperature_value_label, &radbuzz_font_40, LV_PART_MAIN);
-    lv_obj_set_style_text_color(m_temperature_value_label, lv_color_white(), LV_PART_MAIN);
-    lv_obj_set_style_text_align(m_temperature_value_label, LV_TEXT_ALIGN_RIGHT, LV_PART_MAIN);
-
-
-    m_trip_distance_value_label = lv_label_create(m_screen);
-    lv_obj_align(m_trip_distance_value_label, LV_ALIGN_BOTTOM_LEFT, 0, -10);
-    lv_obj_set_style_text_font(m_trip_distance_value_label, &radbuzz_font_40, LV_PART_MAIN);
-    lv_obj_set_style_text_color(m_trip_distance_value_label, lv_color_white(), LV_PART_MAIN);
-
-    m_trip_distance_label = lv_label_create(m_screen);
-    lv_obj_align_to(
-        m_trip_distance_label, m_trip_distance_value_label, LV_ALIGN_OUT_TOP_LEFT, 0, -4);
-    lv_obj_set_style_text_font(m_trip_distance_label, &radbuzz_font_22, LV_PART_MAIN);
-    lv_obj_set_style_text_color(m_trip_distance_label, lv_color_white(), LV_PART_MAIN);
-    lv_obj_set_style_text_align(m_trip_distance_label, LV_TEXT_ALIGN_LEFT, LV_PART_MAIN);
-    lv_label_set_text(m_trip_distance_label, "Trip");
-
+    m_range = BottomRight("Range", "999", "km");
 
     m_power_label = lv_label_create(m_screen);
     lv_obj_align(m_power_label, LV_ALIGN_BOTTOM_MID, -16, -10);
     lv_obj_set_style_text_font(m_power_label, &radbuzz_font_40, LV_PART_MAIN);
     lv_obj_set_style_text_align(m_power_label, LV_TEXT_ALIGN_RIGHT, LV_PART_MAIN);
     lv_obj_set_style_text_color(m_power_label, lv_color_white(), LV_PART_MAIN);
-
-
-    m_range_value = lv_label_create(m_screen);
-    lv_obj_align(m_range_value, LV_ALIGN_BOTTOM_RIGHT, -20, -10);
-    lv_obj_set_style_text_font(m_range_value, &radbuzz_font_40, LV_PART_MAIN);
-    lv_obj_set_style_text_align(m_range_value, LV_TEXT_ALIGN_RIGHT, LV_PART_MAIN);
-    lv_obj_set_style_text_color(m_range_value, lv_color_white(), LV_PART_MAIN);
-
-    m_range_label = lv_label_create(m_screen);
-    lv_obj_set_style_text_font(m_range_label, &radbuzz_font_22, LV_PART_MAIN);
-    lv_obj_set_style_text_align(m_range_label, LV_TEXT_ALIGN_RIGHT, LV_PART_MAIN);
-    lv_obj_set_style_text_color(m_range_label, lv_color_white(), LV_PART_MAIN);
-    lv_label_set_text(m_range_label, "Range");
 }
 
 void
@@ -108,7 +158,7 @@ SpeedometerOnlyScreen::Update()
     lv_label_set_text(m_speedometer_label,
                       std::format("{}", m_parent.m_state.Get<AS::speed>()).c_str());
 
-    lv_label_set_text(m_battery_value_label,
+    lv_label_set_text(m_battery.value_label,
                       std::format("{}", m_parent.m_state.Get<AS::battery_soc>()).c_str());
 
     std::string temperature_text = "Controller";
@@ -129,37 +179,41 @@ SpeedometerOnlyScreen::Update()
                                   std::to_string(bms->highest_cell_temp);
     }
 
-    lv_label_set_text(m_temperature_label, temperature_text.c_str());
-    lv_label_set_text(m_temperature_value_label, temperature_value_text.c_str());
+    lv_label_set_text(m_temperature.description_label, temperature_text.c_str());
+    lv_label_set_text(m_temperature.value_label, temperature_value_text.c_str());
 
     lv_label_set_text(m_power_label,
                       std::format("{} W", m_parent.m_state.Get<AS::current_power_w>()).c_str());
 
-    lv_label_set_text(m_range_value,
-                      std::format("{} km", m_parent.m_state.Get<AS::estimated_range_km>()).c_str());
+    lv_label_set_text(m_range.value_label,
+                      std::format("{}", m_parent.m_state.Get<AS::estimated_range_km>()).c_str());
 
     std::string trip_value_text;
     auto trip_distance_m = m_parent.m_state.Get<AS::trip_distance>();
 
     if (trip_distance_m < 1000)
     {
-        trip_value_text = std::format("{} m", trip_distance_m);
+        trip_value_text = std::format("{}", trip_distance_m);
+        lv_label_set_text(m_trip_distance.value_unit_label, "m");
     }
     else
     {
-        trip_value_text = std::format("{:.1f} km", trip_distance_m / 1000.0f);
+        trip_value_text = std::format("{:.1f}", trip_distance_m / 1000.0f);
+        lv_label_set_text(m_trip_distance.value_unit_label, "km");
     }
-    trip_value_text += ", " + SecondsToString(m_parent.m_state.Get<AS::trip_duration>());
+    // trip_value_text += ", " + SecondsToString(m_parent.m_state.Get<AS::trip_duration>());
 
-    lv_label_set_text(m_trip_distance_value_label, trip_value_text.c_str());
+    lv_label_set_text(m_trip_distance.value_label, trip_value_text.c_str());
 
 
     // Dynamic alignment
     lv_obj_align_to(
-        m_temperature_value_label, m_temperature_value_unit_label, LV_ALIGN_OUT_LEFT_BOTTOM, -4, 2);
-    lv_obj_align_to(
-        m_trip_distance_label, m_trip_distance_value_label, LV_ALIGN_OUT_TOP_LEFT, 0, -4);
-    lv_obj_align_to(m_range_label, m_range_value, LV_ALIGN_OUT_TOP_RIGHT, 0, -4);
+        m_temperature.value_label, m_temperature.value_unit_label, LV_ALIGN_OUT_LEFT_BOTTOM, -4, 2);
+    lv_obj_align_to(m_trip_distance.description_label,
+                    m_trip_distance.value_label,
+                    LV_ALIGN_OUT_TOP_LEFT,
+                    0,
+                    -4);
 }
 
 void
--- src/user_interface/speedometer_only_screen.hh
+++ src/user_interface/speedometer_only_screen.hh
@@ -5,6 +5,13 @@
 class SpeedometerOnlyScreen : public UserInterface::ScreenBase
 {
 public:
+    struct Datum
+    {
+        lv_obj_t* description_label {nullptr};
+        lv_obj_t* value_label {nullptr};
+        lv_obj_t* value_unit_label {nullptr};
+    };
+
     explicit SpeedometerOnlyScreen(UserInterface& parent);
 
 private:
@@ -12,25 +19,20 @@ private:
     void HandleInput(const Input::Event& event) final;
     void SetHelp(bool on) final;
 
+    Datum TopRight(const char* label_text, const char* unit_text);
+    Datum TopLeft(const char* label_text, const char* unit_text);
+    Datum BottomLeft(const char* label_text, const char *value_text, const char* unit_text);
+    Datum BottomRight(const char* label_text, const char *value_text, const char* unit_text);
+
     lv_obj_t* m_speedometer_label {nullptr};
     lv_obj_t* m_speedometer_unit_label {nullptr};
 
     lv_obj_t* m_power_label {nullptr};
 
-    lv_obj_t* m_battery_label {nullptr};
-    lv_obj_t* m_battery_value_label {nullptr};
-    lv_obj_t* m_battery_value_unit_label {nullptr};
-
-    lv_obj_t* m_temperature_label {nullptr};
-    lv_obj_t* m_temperature_value_label {nullptr};
-    lv_obj_t* m_temperature_value_unit_label {nullptr};
-
-    lv_obj_t* m_trip_distance_label {nullptr};
-    lv_obj_t* m_trip_distance_value_label {nullptr};
-    lv_obj_t* m_trip_time_label {nullptr};
+    Datum m_battery;
+    Datum m_temperature;
+    Datum m_trip_distance;
+    Datum m_range;
 
     lv_obj_t* m_consumption_label {nullptr};
-
-    lv_obj_t* m_range_label {nullptr};
-    lv_obj_t* m_range_value {nullptr};
 };
